1f98f4c622908a6f6ec8e12253165e793a860957
exitshadow <exertingshadows@gmail.com>
2023-06-11 00:05:08 +0200

    created the scripts

---
+++ Assets/Scripts/Core Managers/UIManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Dispatches all the texts in the game into their right places in the UI and manages opening and closing UI groups.
+/// </summary>
+///
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
