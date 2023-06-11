ffa72620447c416e4c3d09f2dc564497e3689b72
exitshadow <exertingshadows@gmail.com>
2023-06-11 13:32:50 +0200

    first draft of managing scripts

--- Assets/Scripts/Core Managers/GameManager.cs
+++ Assets/Scripts/Core Managers/GameManager.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The Game Manager is a script that interfaces with a Game State Asset.
+/// It is meant to be reached by other scripts in the scene that inform it
+/// of the changes that have to be done. Modification of the asset is only
+/// done by the GameManager.
+/// </summary>
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameStateAsset gameStateData;
+
+    /// <summary>
+    /// To be called by a Unity Event after the first dad conversation.
+    /// </summary>
+    public void ConfirmTalkDadAtHome()
     {
-        
+        gameStateData.hasSpokenToDadAtHome = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// To be called by a Unity Event after the first neighbour conversation.
+    /// </summary>
+    public void ConfirmReceptionNeighbourFirstCall()
     {
-        
+        gameStateData.hasReceivedNeighbourFirstCall = true;
     }
+
+    /// <summary>
+    /// To be called by a Unity Event after having spoken to the neighbour in front of her house.
+    /// </summary>
+    public void ConfirmVisitNeighbourFirstTime()
+    {
+        gameStateData.hasVisitedNeighbour = true;
+    }
+
+    /// <summary>
+    /// To be called by a Unity Event after having found the tree branch.
+    /// </summary>
+    public void ConfirmFoundTreeBranch()
+    {
+        gameStateData.hasVisitedNeighbour = true;
+    }
+
+    /// <summary>
+    /// To be called by a Unity Event after having captured the butterflies.
+    /// </summary>
+    public void ConfirmButterflyCapture()
+    {
+        gameStateData.hasVisitedNeighbour = true;
+    }
+
+    /// <summary>
+    /// To be called by a Unity Event after having found the cat blocking the alley.
+    /// </summary>
+    public void ConfirmCatEncounter()
+    {
+        gameStateData.hasEncounteredCat = true;
+    }
+
+    /// <summary>
+    /// To be called by a Unity Event after having found tuna cans.
+    /// </summary>
+    public void ConfirmTunaCansFound()
+    {
+        gameStateData.hasFoundTuna = true;
+    }
+
+    /// <summary>
+    /// To be called by a Unity Event after having fed the cat.
+    /// </summary>
+    public void ConfirmCatIsFed()
+    {
+        gameStateData.hasFoundTuna = true;
+    }
+
 }
--- Assets/Scripts/Core Managers/Scriptable Objects/InventoryStateAsset.cs
+++ Assets/Scripts/Core Managers/Scriptable Objects/InventoryStateAsset.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Simple class that represents the inventory of the player. We opted to track a
+/// number of items with ints for scope purposes. All changes in this asset
+/// should be done by the InventoryManager class.
+/// This system should be replaced in the case the game increases in scope.
+/// </summary>
+[CreateAssetMenu(menuName = "Inventory Asset", fileName = "Inventory Asset")]
 public class InventoryStateAsset : ScriptableObject
 {
-
+    public int treeBranchesAmount = 0;
+    public int tunaCansAmount = 0;
 }
