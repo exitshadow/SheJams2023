a185340f3d4a9d97c69153bdb31b2ae679a7d394
exitshadow <exertingshadows@gmail.com>
2023-06-11 15:31:17 +0200

    installed Cinemachine + TextAnimator

--- Assets/Scripts/Annoying Phone Feature/AnnoyingTextMessageAsset.cs
+++ Assets/Scripts/Annoying Phone Feature/AnnoyingTextMessageAsset.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Asset class holding the blueprint for all information needed for the phone text messages.
+/// </summary>
+[CreateAssetMenu(menuName ="Handiman Data Assets/Annoying Phone Messages Asset")]
 public class AnnoyingTextMessageAsset : ScriptableObject
 {
-
+    [System.Serializable]
+    public struct TextMessage
+    {
+        // todo
+    }
 }
