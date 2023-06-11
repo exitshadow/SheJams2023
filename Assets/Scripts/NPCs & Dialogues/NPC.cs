3432440c93c64efaa42666d2c51c9ec38c0c9eda
CaptainVanessa <vanessa.coeurnelle@gmail.com>
2023-06-11 17:38:29 +0200

    continued npc dialogue system

--- Assets/Scripts/NPCs & Dialogues/NPC.cs
+++ Assets/Scripts/NPCs & Dialogues/NPC.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 /// <summary>
 /// abstract class NPC of which they inherit common behaviour such as the lines of the dialogue and the management of their animations
@@ -8,14 +9,35 @@ using UnityEngine;
 public class NPC : MonoBehaviour
 {
     [SerializeField]
+    protected NPCDialogueAsset npcDialogueData;
+    protected Queue<NPCDialogueAsset.DialogueSegment> dialogueQueued = new Queue<NPCDialogueAsset.DialogueSegment>();
+    protected GameManager gameManager;
+    protected UIManager uiManager;
+    // subscription to a delegate that listens to player interacting actions ?
 
-    void Start()
-    {
-        
+    protected Animator npcAnimator;
+    protected Collider npcTriggerCollider;
+    protected NavMeshAgent npcNavMeshAgent;
+    private bool isPlayingDialogue;
+
+    public void FetchDialogue(){ // from the NPCDialogueAsset
+        dialogueQueued.Clear();
+
     }
 
-    void Update()
-    {
-        
+    public void InjectDialogue(){ // into the UIManager
+        if (dialogueQueued.Count == 0){
+            // End Dialogue
+            return;
+        }
+        NPCDialogueAsset.DialogueSegment currentDialogue = dialogueQueued.Dequeue();
+        // send currentDialogue to UI Manager
+        // play "talking" sound?
+    }
+
+    public virtual void InitializeDialogue(NPCDialogueAsset _dialogue){ // specifics are defined by the inherited class
+
+        // set npcAnimator to talking
+
     }
 }
--- Assets/Scripts/NPCs & Dialogues/NPCDialogueAsset.cs
+++ Assets/Scripts/NPCs & Dialogues/NPCDialogueAsset.cs
@@ -8,21 +8,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Handiman Data Assets/NPC Dialogue Asset", fileName = "NPC Dialogue Asset")]
 public class NPCDialogueAsset : ScriptableObject
 {
-    public string dialogueName;
+    [Header("Quest start dialogue")]
+    public List<DialogueSegment> questStartingDialogueSegments = new List<DialogueSegment>();
+    public List<DialogueSegment> questWaitingDialogueSegments = new List<DialogueSegment>();
 
-    [Header("First dialogue")]
-    public List<DialogueSegment> firstDialogueSegments = new List<DialogueSegment>();
-
-    [Header("Quest dialogue")]
-    public List<DialogueSegment> questDialogueSegments = new List<DialogueSegment>();
-    public List<DialogueSegment> recapDialogueSegments = new List<DialogueSegment>();
-
-    [Header("Quest done dialogue")]
-    public List<DialogueSegment> endDialogueSegments = new List<DialogueSegment>();
+    [Header("Quest end dialogue")]
+    public List<DialogueSegment> questProgressingDialogueSegments = new List<DialogueSegment>();
+    public List<DialogueSegment> questEndingDialogueSegments = new List<DialogueSegment>();
 
     [System.Serializable]
     public struct DialogueSegment{
+        [Tooltip("Please specify the speaker's name every time")]
+        public string speakerName;
         public string dialogueText;
-        // public List<DialogueChoice> dialogueChoices;
     }
 }
