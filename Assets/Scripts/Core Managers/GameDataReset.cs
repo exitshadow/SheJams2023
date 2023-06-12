d64e5496d16eee8278c37d2b8b250e96897c94cc
exitshadow <exertingshadows@gmail.com>
2023-06-13 00:32:32 +0200

    wrote reset script, has to be integrated in the start new game

---
+++ Assets/Scripts/Core Managers/GameDataReset.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataReset : MonoBehaviour
+{
+    [SerializeField] GameStateAsset gameState;
+
+    private void Start()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        gameState.hasSpokenToDadAtHome = false;
+        gameState.hasReceivedNeighbourFirstCall = false;
+        gameState.hasVisitedNeighbour = false;
+        gameState.hasFoundTreeBranch = false;
+
+        gameState.hasEncounteredCat = false;
+        gameState.hasFoundTuna = false;
+        gameState.hasSpokenToVet = false;
+        gameState.hasFedCat = false;
+
+        gameState.hasResetDadsComputer = false;
+    }
+}
