a8538daa6c1259324bcdbf597fc32c0ba4b9518f
exitshadow <exertingshadows@gmail.com>
2023-06-13 18:34:21 +0200

    finished first version of text messages

--- Assets/Scripts/Core Managers/UIManager.cs
+++ Assets/Scripts/Core Managers/UIManager.cs
@@ -118,7 +118,7 @@ public class UIManager : MonoBehaviour
 
         // append as child to the vertical layout group
         messageSnippetBox.transform.SetParent(messageTextArea.transform);
-        messageSnippetBox.transform.localRotation = Quaternion.Euler(0,0,-4);
+        messageSnippetBox.transform.localRotation = Quaternion.Euler(0,0,0);
 
         // apply the text to the TMP component in the prefab’s children
         messageSnippetText = messageSnippetBox.GetComponentInChildren<TextMeshProUGUI>();
@@ -155,9 +155,14 @@ public class UIManager : MonoBehaviour
         float newWidth;
         float newHeight;
 
+        int maxCharsFirstLine;
+
         int nbLines = (int)Mathf.Ceil(text.Length / maxCharsPerLine);
 
-        newWidth = (charWidth * text.Length) + (2 * messageHorizontalMargin);
+        if (nbLines < 2 ) maxCharsFirstLine = text.Length;
+        else maxCharsFirstLine = maxCharsPerLine;
+
+        newWidth = (charWidth * maxCharsFirstLine) + (2 * messageHorizontalMargin);
         newHeight = (nbLines * charHeight * 2) + (2 * messageVerticalMargin);
 
         parentRT.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newHeight);
