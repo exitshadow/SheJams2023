532d57cf3a486a9829b01d263cb01aa2051fe253
exitshadow <exertingshadows@gmail.com>
2023-09-05 16:29:12 +0200

    trying to write a nice debug editor but bleh

--- Assets/_Scripts/Core Managers/GameState.cs
+++ Assets/_Scripts/Core Managers/GameState.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using System;
 
 /// <summary>
 /// Class that syncs the yarn and scriptable assets game states.
@@ -12,9 +15,42 @@ public class GameState : MonoBehaviour
     [SerializeField] private InMemoryVariableStorage yarnState;
     [SerializeField] private GameVariablesStorage gameState;
 
+    public event Action onSetValuesFromGameToYarn;
+    public event Action onSetValuesFromYarnToGame;
+    public event Action onSetValue;
+
+    [HideInInspector] public List<string> variableNames = new List<string>();
+    [HideInInspector] public List<string> variableValues = new List<string>();
+
+    private void OnEnable()
+    {
+        onSetValue += OnDataChange;
+        onSetValuesFromGameToYarn += OnDataChange;
+        onSetValuesFromYarnToGame += OnDataChange;
+    }
+
+    void OnDisable()
+    {
+        onSetValue -= OnDataChange;
+        onSetValuesFromGameToYarn -= OnDataChange;
+        onSetValuesFromYarnToGame -= OnDataChange;
+    }
+
     private void Start()
     {
         SetValuesFromGameToYarn();
+        RefreshVariablesViewingLists();
+    }
+
+    private void RefreshVariablesViewingLists()
+    {
+        variableNames.Clear();
+        variableValues.Clear();
+        foreach (var boolVar in gameState.boolVariables)
+        {
+            variableValues.Add(boolVar.Key);
+            variableNames.Add(boolVar.Value.ToString());
+        }
     }
 
     public string MissionStatusText
@@ -43,11 +79,12 @@ public class GameState : MonoBehaviour
             if (yarnState.TryGetValue(gameVar.Key, out string s))
                 yarnState.SetValue(gameVar.Key, gameVar.Value);
         }
-            
+
+        onSetValuesFromGameToYarn?.Invoke();
     }
 
     public void SetValuesFromYarnToGame()
-    {
+    { 
         (var floats, var strings, var bools) = yarnState.GetAllVariables();
 
         foreach (var yarnBool in bools)
@@ -58,7 +95,23 @@ public class GameState : MonoBehaviour
             }
         }
 
+        foreach (var yarnFloat in floats)
+        {
+            if (gameState.floatVariables.TryGetValue(yarnFloat.Key, out float f))
+            {
+                gameState.floatVariables[yarnFloat.Key] = f;
+            }
+        }
+
+        foreach (var yarnString in strings)
+        {
+            if (gameState.stringVariables.TryGetValue(yarnString.Key, out string s))
+            {
+                gameState.stringVariables[yarnString.Key] = s;
+            }
+        }
         
+        onSetValuesFromYarnToGame?.Invoke();
     }
 
     public void SetValue(string key, bool value)
@@ -67,6 +120,7 @@ public class GameState : MonoBehaviour
         {
             yarnState.SetValue(key, value);
             gameState.boolVariables[key] = value;
+            onSetValue?.Invoke();
         }
         else Debug.Log($"No variable with the key {key} has been found in yarn storage");
     }
@@ -77,6 +131,7 @@ public class GameState : MonoBehaviour
         {
             yarnState.SetValue(key, value);
             gameState.stringVariables[key] = value;
+            onSetValue?.Invoke();
         }
         else Debug.Log($"No variable with the key {key} has been found in yarn storage");
     }
@@ -87,7 +142,13 @@ public class GameState : MonoBehaviour
         {
             yarnState.SetValue(key, value);
             gameState.floatVariables[key] = value;
+            onSetValue?.Invoke();
         }
         else Debug.Log($"No variable with the key {key} has been found in yarn storage");
+    }
+
+    public void OnDataChange()
+    {
+        RefreshVariablesViewingLists();
     }
 }
