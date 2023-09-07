6039fe8b0f6b292b34354a504dc391d99f223e63
Tylia <shailadevi_mahaux@yahoo.fr>
2023-09-07 11:22:28 +0200

    add IK

---
+++ Assets/_Scripts/NPCs & Dialogues/IKLookatAnimation.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class IKLookatAnimation : MonoBehaviour
+{
+    #region Variables
+    private Rig headRig;
+    //[SerializeField] private RigBuilder rigBuilder;
+    private float targetWeight;
+    private WeightedTransformArray sources;
+
+    private Coroutine currentCoroutine;
+    #endregion
+
+    #region Méthodes
+    void Start()
+    {
+        headRig = GetComponent<Rig>();
+        //sources = headRig.data.sourceObjects;
+    }
+
+    IEnumerator TurnHead(float targetWeight)
+    {
+        Debug.Log("inCoroutine");
+        float actionTime = 1f;
+        float timer = 0f;
+
+        while (timer < actionTime)
+        {
+            float ratio = timer/actionTime;
+            yield return new WaitForEndOfFrame();
+            headRig.weight = Mathf.Lerp(headRig.weight, targetWeight, ratio);
+            //headRig.data.sourceObjects.SetWeight(0,Mathf.Lerp(headRig.weight, targetWeight, ratio));
+            Debug.Log(headRig.weight);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        headRig.weight = targetWeight;
+        Debug.Log(headRig.weight);
+    }
+
+  
+    
+    public void ActivateLookat()
+    {
+        Debug.Log("Activating");
+        if (currentCoroutine != null)
+        {
+            StopCoroutine(currentCoroutine); 
+        }
+        currentCoroutine = StartCoroutine(TurnHead(1));
+    }
+
+    public void DeactivateLookat()
+    {
+        Debug.Log("Deactivating");
+        if (currentCoroutine != null)
+        {
+            StopCoroutine(currentCoroutine); 
+        }
+        currentCoroutine = StartCoroutine(TurnHead(0));
+    }
+     #endregion
+}
