0f5d75a69f898d30ac602033d73ba9323d0b3c04
Tylia <shailadevi_mahaux@yahoo.fr>
2023-09-09 22:36:33 +0200

    IK system and Cat animation

--- Assets/_Scripts/NPCs & Dialogues/IKLookatAnimation.cs
+++ Assets/_Scripts/NPCs & Dialogues/IKLookatAnimation.cs
@@ -7,9 +7,7 @@ public class IKLookatAnimation : MonoBehaviour
 {
     #region Variables
     private Rig headRig;
-    //[SerializeField] private RigBuilder rigBuilder;
-    private float targetWeight;
-    private WeightedTransformArray sources;
+    //private float targetWeight;
 
     private Coroutine currentCoroutine;
     #endregion
@@ -18,60 +16,44 @@ public class IKLookatAnimation : MonoBehaviour
     void Start()
     {
         headRig = GetComponent<Rig>();
-        Debug.LogWarning(headRig);
-        //sources = headRig.data.sourceObjects;
     }
 
-    void Update()
-    {
-        //todo wtf??????????????????????
-        // headRig.weight += Time.deltaTime;
-    }
+
 
     IEnumerator TurnHead(float targetWeight)
     {
-        Debug.Log("inCoroutine");
         float actionTime = 1f;
         float timer = 0f;
 
         while (timer < actionTime)
         {
             float ratio = timer/actionTime;
-            yield return new WaitForEndOfFrame();
+            //yield return new WaitForEndOfFrame();
             headRig.weight = Mathf.Lerp(headRig.weight, targetWeight, ratio);
-            //headRig.data.sourceObjects.SetWeight(0,Mathf.Lerp(headRig.weight, targetWeight, ratio));
-            Debug.Log(headRig.weight);
             timer += Time.deltaTime;
             yield return null;
         }
         headRig.weight = targetWeight;
-        Debug.Log(headRig.weight);
     }
 
   
     
     public void ActivateLookat()
     {
-        Debug.Log("Activating");
         if (currentCoroutine != null)
         {
             StopCoroutine(currentCoroutine); 
         }
         currentCoroutine = StartCoroutine(TurnHead(1));
-
-        headRig.weight = 1f;
     }
 
     public void DeactivateLookat()
     {
-        Debug.Log("Deactivating");
         if (currentCoroutine != null)
         {
             StopCoroutine(currentCoroutine); 
         }
         currentCoroutine = StartCoroutine(TurnHead(0));
-
-        headRig.weight = 0f;
     }
      #endregion
 }
