dc947323dd8d0544b630996c4e0fdd4bb3dd81e9
exitshadow <exertingshadows@gmail.com>
2023-09-12 12:56:39 +0200

    implemented ik lookat on animated characters

--- Assets/_Scripts/NPCs & Dialogues/AnimatedNPC.cs
+++ Assets/_Scripts/NPCs & Dialogues/AnimatedNPC.cs
@@ -1,22 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using Yarn.Unity;
 
 [RequireComponent(typeof(Animator))]
 public abstract class AnimatedNPC : NPC
 {
+    [Header("IK LookAt Options")]
+    [SerializeField] protected bool useLookAtOnTrigger;
     protected Animator animator;
+    protected IKLookatAnimation lookAt;
 
     protected override void Awake()
     {
         base.Awake();
 
         SetAnimator();
+        GetIKRig();
+
     }
 
     protected virtual void SetAnimator()
     {
         animator = GetComponent<Animator>();
         animator.SetLayerWeight(2, 0);
+    }
+
+    protected void GetIKRig()
+    {
+        lookAt = GetComponentInChildren<IKLookatAnimation>();
+    }
+
+    [YarnCommand("enable_look_at")]
+    public void EnableLookAt()
+    {
+        lookAt.ActivateLookat();
+    }
+
+    [YarnCommand("disable_look_at")]
+    public void DisableLookAt()
+    {
+        lookAt.DeactivateLookat();
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+        
+        if (player && useLookAtOnTrigger)
+        {
+            EnableLookAt();
+        }
+    }
+
+    protected override void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+
+        if (player)
+        {
+            DisableLookAt();
+        }
     }
 }
--- Assets/_Scripts/NPCs & Dialogues/IKLookatAnimation.cs
+++ Assets/_Scripts/NPCs & Dialogues/IKLookatAnimation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
+[RequireComponent(typeof(Rig))]
 public class IKLookatAnimation : MonoBehaviour
 {
     #region Variables
