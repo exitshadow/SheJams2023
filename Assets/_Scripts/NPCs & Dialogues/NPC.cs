52f59d5b4225daa44a4a6f906c22d0b74cecc152
exitshadow <exertingshadows@gmail.com>
2024-05-20 15:26:56 +0200

    cleanup

--- Assets/_Scripts/NPCs & Dialogues/NPC.cs
+++ Assets/_Scripts/NPCs & Dialogues/NPC.cs
@@ -1,8 +1,4 @@
-using System.Globalization;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using Yarn.Unity;
