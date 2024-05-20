fdf7d545641f082ac2b2311822c3d7978629d8a7
exitshadow <exertingshadows@gmail.com>
2024-05-20 15:50:50 +0200

    cleanup of old GameManager class & marked as deprecated

--- Assets/_Scripts/Core Scripts/Game State Tracking/Old/GameManager.cs
+++ Assets/_Scripts/Core Scripts/Game State Tracking/Old/GameManager.cs
@@ -1,169 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
 /// <summary>
-/// The Game Manager is a script that interfaces with a Game State Asset.
-/// It is meant to be reached by other scripts in the scene that inform it
-/// of the changes that have to be done. Modification of the asset is only
-/// done by the GameManager.
+/// Class is obsolete.
 /// </summary>
+[Obsolete("Game Manager class is obsolete, use GameState instead. Class is preserved for the mouse lock and will be replaced.", false)]
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameStateAsset gameStateData;
-    private InMemoryVariableStorage yarnVariablesStorage;
-
-    [SerializeField] private MissionStatusUI missionStatus;
-    [SerializeField] private CutsceneManager cutsceneManager;
-
     private void Start()
     {
-        
-
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void SetAllYarnVariables()
-    {
-        
-    }
-
-    // #region old?
-    // #region dad’s quest
-    // public bool DidSpeakToDadFirstTime()
-    // {
-    //     return gameStateData.hasSpokenToDadAtHome;
-    // }
-
-    // public bool HasCapturedAllButterfies()
-    // {
-    //     return gameStateData.hasCapturedButterflies;
-    // }
-
-    // public bool HasResetDadsComputer()
-    // {
-    //     return gameStateData.hasResetDadsComputer;
-    // }
-    // #endregion
-
-    // #region cat’s and neighbour quest
-    // public bool HasVisitedNeighbour()
-    // {
-    //     return gameStateData.hasVisitedNeighbour;
-    // }
-
-    // public bool HasFoundTheButterflyBranch()
-    // {
-    //     return gameStateData.hasFoundTreeBranch;
-    // }
-
-    // public bool HasEncounteredCat()
-    // {
-    //     return gameStateData.hasEncounteredCat;
-    // }
-
-    // public bool HasFoundTuna()
-    // {
-    //     return gameStateData.hasFoundTuna;
-    // }
-
-    // public bool HasFedCat()
-    // {
-    //     return gameStateData.hasFedCat;
-    // }
-
-    // public bool HasSpokenToVet()
-    // {
-    //     return gameStateData.hasSpokenToVet;
-    // }
-    // #endregion
-
-    // /// <summary>
-    // /// To be called by a Unity Event after the first dad conversation.
-    // /// </summary>
-    // public void ConfirmTalkDadAtHome()
-    // {
-    //     gameStateData.hasSpokenToDadAtHome = true;
-    //     missionStatus.ChangeMissionPrompt("Go to speak to the neighbor.");
-    // }
-
-    // public void ConfirmResettingDadsComputer()
-    // {
-    //     gameStateData.hasResetDadsComputer = true;
-    //     missionStatus.ChangeMissionPrompt("Like an subscribe!!");
-    // }
-
-    // /// <summary>
-    // /// To be called by a Unity Event after the first neighbour conversation.
-    // /// </summary>
-    // public void ConfirmReceptionNeighbourFirstCall()
-    // {
-    //     gameStateData.hasReceivedNeighbourFirstCall = true;
-    //     missionStatus.ChangeMissionPrompt("Dad wanted to ask for something.");
-    // }
-
-    // /// <summary>
-    // /// To be called by a Unity Event after having spoken to the neighbour in front of her house.
-    // /// </summary>
-    // public void ConfirmVisitNeighbourFirstTime()
-    // {
-    //     gameStateData.hasVisitedNeighbour = true;
-    //     missionStatus.ChangeMissionPrompt("Go find the butterfly tree branch.");
-    // }
-
-    // /// <summary>
-    // /// To be called by a Unity Event after having found the tree branch.
-    // /// </summary>
-    // public void ConfirmFoundTreeBranch()
-    // {
-    //     gameStateData.hasFoundTreeBranch = true;
-    //     missionStatus.ChangeMissionPrompt("Go back to the neighbor bring her the butterfly tree branch.");
-    // }
-
-    // /// <summary>
-    // /// To be called by a Unity Event after having captured the butterflies.
-    // /// </summary>
-    // public void ConfirmButterflyCapture()
-    // {
-    //     gameStateData.hasCapturedButterflies = true;
-    //     missionStatus.ChangeMissionPrompt("Rembember dad needed you with the computer.");
-    // }
-
-    // /// <summary>
-    // /// To be called by a Unity Event after having found the cat blocking the alley.
-    // /// </summary>
-    // public void ConfirmCatEncounter()
-    // {
-    //     gameStateData.hasEncounteredCat = true;
-    //     missionStatus.ChangeMissionPrompt("Find something for the cat to eat! like tuna!");
-    // }
-
-    // /// <summary>
-    // /// To be called by a Unity Event after having found tuna cans.
-    // /// </summary>
-    // public void ConfirmTunaCansFound()
-    // {
-    //     gameStateData.hasFoundTuna = true;
-    //     missionStatus.ChangeMissionPrompt("Find the cat to give it tuna!");
-    // }
-
-    // /// <summary>
-    // /// To be called by a Unity Event after having fed the cat.
-    // /// </summary>
-    // public void ConfirmCatIsFed()
-    // {
-    //     gameStateData.hasFedCat = true;
-    //     missionStatus.ChangeMissionPrompt("Oh right, the butterfly tree.");
-    // }
-
-    // /// <summary>
-    // /// To be called by a Unity Event after having been scolded by the veterinarian.
-    // /// </summary>
-    // public void ConfirmSpokeToVet()
-    // {
-    //     gameStateData.hasSpokenToVet = true;
-    // }
-    // #endregion;
 }
